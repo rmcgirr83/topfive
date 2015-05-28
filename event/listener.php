543db4b072529c659d8bf1c029b0a607c4cb82e7
RMcGirr83 <rmcgirr83@gmail.com>
2015-05-28 09:15:09 -0400

    Change to proper name spacing fix rtl

--- event/listener.php
+++ event/listener.php
@@ -19,8 +19,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
-	/* @var \rmcgirr83\topfive\core\functions_topfive */
-	protected $tf_functions;
+	/* @var \rmcgirr83\topfive\core\topfive */
+	protected $functions;
 
 	/** @var \phpbb\config\config */
 	protected $config;
@@ -31,9 +31,9 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\user */
 	protected $user;
 
-	public function __construct(\rmcgirr83\topfive\core\functions_topfive $functions, \phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user)
+	public function __construct(\rmcgirr83\topfive\core\topfive $functions, \phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user)
 	{
-		$this->tf_functions = $functions;
+		$this->functions = $functions;
 		$this->config = $config;
 		$this->template = $template;
 		$this->user = $user;
@@ -57,9 +57,9 @@ class listener implements EventSubscriberInterface
 		// add lang file
 		$this->user->add_lang_ext('rmcgirr83/topfive', 'topfive');
 
-		$this->tf_functions->topposters();
-		$this->tf_functions->newusers();
-		$this->tf_functions->toptopics();
+		$this->functions->topposters();
+		$this->functions->newusers();
+		$this->functions->toptopics();
 
 		$this->template->assign_vars(array(
 			'S_TOPFIVE'	=>	$this->config['top_five_active'],
